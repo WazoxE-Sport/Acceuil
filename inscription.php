b752da4f827173777facd990816b7ad7a68a27a7
WazoxE-Sport <74671347+WazoxE-Sport@users.noreply.github.com>
2020-11-22 20:05:36 +0100

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -44,8 +44,67 @@
 
     <div class="container">
         <div class="form">
+        <?php
+        if(isset($_GET['reg_err']))
+        {
+            $err = htmlspecialchars($_GET['reg_err']);
+
+            switch($err)
+            {
+                case 'sucess':
+                ?>
+                    <div class="alert alert-success">
+                        <strong>Succès</strong>Inscription réussie !
+                </div>
+                <?php
+                break;
+                
+                case 'password':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Succès</strong>mot de passe différent
+                </div>
+                <?php
+                break;
+
+                case 'email':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Succès</strong>email non valide
+                </div>
+                <?php
+                break;
+
+                case 'email_length':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Succès</strong>email trop long
+                </div>
+                <?php
+                break;
+
+                case 'pseudo_length':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Succès</strong>pseudo trop long
+                </div>
+                <?php
+                break;
+
+                case 'already':
+                ?>
+                    <div class="alert alert-danger">
+                        <strong>Succès</strong>compte deja existant
+                </div>
+                <?php
+                break;
+
+                    
+            }
+        }
+        ?>
             <div class="form__title">Inscription</div>
-            <form class="form__inner" action="">
+            <form class="form__inner" action="inscription_traitement.php">
               <div class="form__line">
                  <div class="form__block">
                     <h4 class="form__label">Nom</h4>
@@ -70,32 +129,6 @@
             </form>
         </div>
     </div>
-
-<!-- Contact -->
-<div class="block">
-    <footer class="footer">
-        <div class="footer-informations">
-            <ul>
-                <li>
-                    <a href="#">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                </li>
-                <li>
-                <a href="https://www.youtube.com/channel/UClWXgoSNJonqZca67hdl3gg">
-                    <i class="fab fa-youtube"></i>
-                </a>
-                </li>
-                <li>
-                <a href="https://discord.gg/Q9s48Ut">
-                    <i class="fab fa-discord"></i>
-                </a>
-                </li>
-            </ul>
-        </div>
-    </footer>
-</div>
-<!-- Contact -->
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="public/css/img/js/app.js"></script>
