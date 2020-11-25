1a1974e43d7242254318c432cc94b2df096ee61c
WazoxE-Sport <74671347+WazoxE-Sport@users.noreply.github.com>
2020-11-25 07:57:23 +0100

    Add files via upload

--- public/css/img/js/app.js
+++ public/css/img/js/app.js
@@ -3,6 +3,28 @@ $('.header__navbar-toggle').click(function(e) {
     $('.header__navbar').toggleClass('is-open');
 })
 
+const header__navbar = document.querySelector('.header__navbar-toggle');
+
+header__navbar.addEventListener('click',() => {
+    header__navbar.classList.toggle('active');
+});
+
+const btn = document.querySelectorAll('.btn');
+const container = document.querySelectorAll('.container')
+const articles = document.querySelectorAll('.articles');
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    .from(container, 1, {top: -50, opacity: 0, ease: "pawer2.out"}, 0.3)
+    .from(btn, 1, {opacity: 0, ease: "pawer2.out"}, 0.3, '-=1')
+    .from(articles, 1, {width: 0, ease: "power2out"}, '-=2');
+
+    TL.play();
+});
+
 {
    // send mail with ajax
    $('#send_email').click(function(e){
